daa728b33b099b84a5b9f3ab101f6dabd5262922
冯兴占 <fengxingzhan@hikvision.com>
2018-11-27 14:32:53 +0800

    增加生产环境配置

--- config/webpack/webpack.file.conf.js
+++ config/webpack/webpack.file.conf.js
@@ -6,3 +6,13 @@ module.exports = {
     resource: 'resource',
     /*静态资源*/
 };
+module.exports = {
+    devDirectory: 'devBuild',
+    /*开发目录*/
+    proDirectory: 'dist',
+    /*发布目录*/
+    resource: 'resource',
+    /*静态资源*/
+    resourcePrefix: 'resource',
+    /*静态资源前缀*/
+};
